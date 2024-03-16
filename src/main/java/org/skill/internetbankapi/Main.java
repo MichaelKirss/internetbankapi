31748b536dc026e9e051945e809fc249db969030
Michael Kirss <Michaelkirss@gmail.com>
2024-03-16 22:12:43 +0400

    Delete Other Comment Main

--- src/main/java/org/skill/internetbankapi/Main.java
+++ src/main/java/org/skill/internetbankapi/Main.java
@@ -1,7 +1,5 @@
 package org.skill.internetbankapi;
 
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,23 +9,4 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-
-//        System.out.println("Hello world!");
-//
-//        Configuration configuration = new Configuration().configure().addAnnotatedClass(UserAccount.class);
-//
-//        SessionFactory sessionFactory = configuration.buildSessionFactory();
-//        Session session = sessionFactory.getCurrentSession();
-//        Transaction tx= session.beginTransaction();
-//
-//        UserAccount userAccount = (UserAccount) session.get(UserAccount.class, new Long(2L));
-//        System.out.println("_______________________________________");
-//        System.out.println(userAccount.getBalance());
-//        System.out.println("_______________________________________");
-//
-//        tx.commit();
-//        sessionFactory.close();
-
-
-
 }
